2fe8003ee1b4a3038c994651111b8ae13f415c27
SenpaiStudios <143940488+SenpaiStudios@users.noreply.github.com>
2024-02-18 17:00:26 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,79 @@
+// Navigation Bar Effects on Scroll
+window.addEventListener("scroll", function(){
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// Services Section-Modal
+const serviceModals = document.querySelectorAll(".service-modal"); 
+const learnmoreBtns = document.querySelectorAll(".learn-more-btn"); 
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = function(modalClick){
+    serviceModals[modalClick].classList.add("active");
+}
+
+learnmoreBtns.forEach((learnmoreBtn, i) => { 
+    learnmoreBtn.addEventListener("click", () => {
+        modal(i);
+    });
+});
+    
+modalCloseBtns.forEach((modalCloseBtn) => {
+    modalCloseBtn.addEventListener("click", () => {
+        serviceModals.forEach((modalView) => {
+            modalView.classList.remove("active")
+        });
+    });
+});
+
+//Portfolio section - Modal
+const portfolioModals = document.querySelectorAll(".portfolio-model"); 
+const imgCards = document.querySelectorAll(".img-card"); 
+const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
+
+var portfolioModal = function(modalClick){
+    portfolioModals[modalClick].classList.add("active");
+}
+
+imgCards.forEach((imgCard, i) => { 
+    imgCard.addEventListener("click", () => {
+        portfolioModal(i);
+    });
+});
+    
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+    portfolioCloseBtn.addEventListener("click", () => {
+        portfolioModals.forEach(( portfolioModalView) => {
+            portfolioModalView.classList.remove("active")
+        });
+    });
+});
+
+// Our Clients - swiper
+var swiper = new Swiper(".client-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+// Website dark/light theme
+
+// Scroll to top button
+
+// Navigation menu items active on page scroll
+
+// Responsive navigation menu toggle
+
+// Scroll reveal animations
+// Common reveal options to create reveal animations
+
+// Target elements, and specify options to create reveal animations
